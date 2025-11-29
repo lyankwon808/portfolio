b9343752beb4499a4f0f02ee83d7ebb550448650
lyankwon808 <lyankwon808@gmail.com>
2025-11-29 12:58:13 +0100

    chore(site): update Instagram link to hyukwoo_studio

--- lib/site.ts
+++ lib/site.ts
@@ -10,7 +10,7 @@ const site: Site = {
   name: "Kwon Hyukwoo",
   email: "lyankwon808@gmail.com",
   linkedin: "https://www.linkedin.com/in/hyukwoo-leon-kwon-33190161/",
-  instagram: "https://instagram.com/leon_h_kwon",
+  instagram: "https://www.instagram.com/hyukwoo_studio/",
 };
 
 export default site;
